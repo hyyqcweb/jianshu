61c08dcf847b7f50d5cc4422c3e673536cee0a61
hyyqc <junjie_hyyqc@163.com>
2018-09-12 09:24:17 +0800

    add route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
-import Header from './common/header'
+import Header from './common/header';
+import { BrowserRouter, Route } from 'react-router-dom';
 import store from './store';
 import {Provider} from 'react-redux';
+
+// exact 精准匹配
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Header/>
+	      <div>
+	      	<Header/>
+	        <BrowserRouter>
+	        	<div>
+	        		<Route path='/' exact render={() => <div>home</div>}></Route>
+	        		<Route path='/detail' exact render={() => <div>detail</div>}></Route>
+	        	</div>
+	        </BrowserRouter>
+	      </div>
       </Provider>
     );
   }
