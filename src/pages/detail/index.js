0139bb6adf0b032a8a9f0dff99ac33cc66f944e8
hyyqc <junjie_hyyqc@163.com>
2018-09-13 13:58:43 +0800

    layout detail

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { DetailWrapper, Header, Content } from './style';
 
 class Detail extends Component {
     render() {
         return (
             <div>
-				Detail
+				<DetailWrapper>
+					<Header>「福利向」Python妹子图爬虫</Header>
+					<Content>
+						<img src="https://upload-images.jianshu.io/upload_images/3810775-c85d043e58fe8900.png?imageMogr2/auto-orient/strip%7CimageView2/2/w/635/format/webp" alt=""/>
+						<p>具体的信息懒得一个个去打了。。。反正需要的时候查一查，网络编程程序猿吗。。。知道是什么用就OK了 。。</p>
+						<p>具体的信息懒得一个个去打了。。。反正需要的时候查一查，网络编程程序猿吗。。。知道是什么用就OK了 。。</p>
+						<p>具体的信息懒得一个个去打了。。。反正需要的时候查一查，网络编程程序猿吗。。。知道是什么用就OK了 。。</p>
+						<p>具体的信息懒得一个个去打了。。。反正需要的时候查一查，网络编程程序猿吗。。。知道是什么用就OK了 。。</p>
+					</Content>
+				</DetailWrapper>
 			</div>
         )
     }
