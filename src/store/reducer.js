babe09c5a748a8f064c9d46fb9e0e71d625148b5
hyyqc <junjie_hyyqc@163.com>
2018-09-13 14:19:46 +0800

    add detail store

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux-immutable';
 import { reducer as HeaderReducer } from '../common/header/store';
-import {reducer as HomeReducer } from '../pages/home/store';
+import { reducer as HomeReducer } from '../pages/home/store';
+import { reducer as detailReducer } from '../pages/detail/store';
 
 // combineReducers 数据拆分
 
 export default combineReducers({
-	header: HeaderReducer,
-	home: HomeReducer
+    header: HeaderReducer,
+    home: HomeReducer,
+    detail: detailReducer
 });
