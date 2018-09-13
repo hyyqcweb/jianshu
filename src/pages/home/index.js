153ed359715385672f622ac3fa3daa54357bbd0f
hyyqc <junjie_hyyqc@163.com>
2018-09-13 10:13:32 +0800

    home axios get

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,8 +5,30 @@ import Recommend from './components/Recommend';
 import Writer from './components/Writer';
 import List from './components/List';
 import logoPic from '../../statics/1.jpg';
+import axios from 'axios';
+import { connect } from 'react-redux';
 
 class Home extends Component {
+	constructor(props) {
+		super(props);
+		this.state={}
+	}
+	componentDidMount() {
+		axios.get('/api/home.json')
+		.then((res) => {
+			const result = res.data.data;
+			const action = {
+				type: 'change_home_data',
+				topicList: result.topicList,
+				acticleList: result.acticleList,
+				writerList: result.writerList
+			}
+			this.props.changeHomeData(action);
+		}).catch(error => {
+			console.log(error)
+		})
+	}
+
 	render() {
 		return (
 			<div>
@@ -25,4 +47,10 @@ class Home extends Component {
 		)
 	}
 }
-export default Home; 
+
+const mapDispatch = (dispatch) => ({
+	changeHomeData(action) {
+		dispatch(action);
+	}
+});
+export default connect(null,mapDispatch)(Home); 
--- src/pages/home/store/reducer.js
+++ src/pages/home/store/reducer.js
@@ -4,57 +4,8 @@ import topicImg from '../../../statics/topic';
 
 // immutable
 const defaultState = fromJS({
-	topicList: [
-		{
-			id: 1,
-			title: '旅行·在路上',
-			imgUrl: topicImg.TopicImg06
-		},
-		{
-			id: 2,
-			title: '摄影',
-			imgUrl: topicImg.TopicImg02
-		},
-		{
-			id: 3,
-			title: '自然科普',
-			imgUrl: topicImg.TopicImg03
-		},
-		{
-			id: 4,
-			title: '@IT·互联网',
-			imgUrl: topicImg.TopicImg05
-		},
-		{
-			id: 5,
-			title: '故事',
-			imgUrl: topicImg.TopicImg07
-		},
-		{
-			id: 6,
-			title: '简书电影',
-			imgUrl: topicImg.TopicImg04
-		},
-		{
-			id: 7,
-			title: '手绘',
-			imgUrl: topicImg.TopicImg01
-		},
-	],
-	acticleList: [
-		{
-			id: 1,
-			title: '把握好人际交往的尺度，内心想不快乐都难',
-			desc: '我们终其一生，都在寻找人际交往的那个“尺度”、“界限”到底在哪? 为什么你好心帮了别人，反而让别人觉得很尴尬，好心办成了坏事? 为什么有的人只是...',
-			imgUrl: 'https://upload-images.jianshu.io/upload_images/6693515-8154b2372b08eca9?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240'
-		},
-		{
-			id: 2,
-			title: '把握好人际交往的尺度，内心想不快乐都难',
-			desc: '我们终其一生，都在寻找人际交往的那个“尺度”、“界限”到底在哪? 为什么你好心帮了别人，反而让别人觉得很尴尬，好心办成了坏事? 为什么有的人只是...',
-			imgUrl: 'https://upload-images.jianshu.io/upload_images/6693515-8154b2372b08eca9?imageMogr2/auto-orient/strip|imageView2/1/w/300/h/240'
-		}
-	],
+	topicList: [],
+	acticleList: [],
 	writerList: [],
 	mouseIn: false
 });
@@ -66,8 +17,14 @@ export default (state = defaultState, action) => {
 			return state.set('mouseIn',true);
 		case constants.MOUSE_LEAVE:
 			return state.set('mouseIn',false);
-		case constants.CHANGE_LIST:
-			return state.set('writerList', action.data)
+		// case constants.CHANGE_LIST:
+		// 	return state.set('writerList', action.data)
+		case 'change_home_data':
+			return state.merge({
+				topicList: fromJS(action.topicList),
+				acticleList: fromJS(action.acticleList),
+				writerList: fromJS(action.writerList),
+			})
 		default: 
 			return state;
 	}
